dad39d665b91287790a98a8befbe80c8d470c181
TheSevenPens <TheSevenPens@gmail.com>
2024-06-21 15:24:16 -0700

    Added namespace alias

--- WinTabPainter/BitmapDocument.cs
+++ WinTabPainter/BitmapDocument.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Drawing;
+using SD=System.Drawing;
 
 
 
@@ -15,11 +15,11 @@ namespace WinTabPainter
     public class BitmapDocument : IDisposable
     {
 
-        private Graphics _gfx;
-        private Bitmap _bmp;
+        private SD.Graphics _gfx;
+        private SD.Bitmap _bmp;
 
-        private Graphics Graphics { get { return this._gfx; } }
-        public Bitmap Bitmap { get { return this._bmp; } }
+        private SD.Graphics Graphics { get { return this._gfx; } }
+        public SD.Bitmap Bitmap { get { return this._bmp; } }
 
         int _width;
         int _height;
@@ -29,7 +29,7 @@ namespace WinTabPainter
 
         public BitmapDocument(int width, int height)
         {
-            this._bmp = new Bitmap(width, height);
+            this._bmp = new SD.Bitmap(width, height);
             this._width= width; 
             this._height = height;
             this._gfx = System.Drawing.Graphics.FromImage(this._bmp);
@@ -55,7 +55,7 @@ namespace WinTabPainter
 
         public void Erase()
         {
-            using (var b = new SolidBrush(System.Drawing.Color.White))
+            using (var b = new SD.SolidBrush(System.Drawing.Color.White))
             {
                 this._gfx.FillRectangle(b, 0, 0, this._bmp.Width, this._bmp.Height);
             }
@@ -66,20 +66,20 @@ namespace WinTabPainter
             this._bmp.Save(filename);
         }
 
-        public void FillEllipse(Color color, Rectangle rect)
+        public void FillEllipse(SD.Color color, SD.Rectangle rect)
         {
-            using (Brush brush = new SolidBrush(color))
+            using (SD.Brush brush = new SD.SolidBrush(color))
             {
                 this._gfx.FillEllipse(brush, rect);
 
             }
         }
 
-        public void DrawDabCenteredAt(Color color, Point p, Size s)
+        public void DrawDabCenteredAt(SD.Color color, SD.Point p, SD.Size s)
         {
             var dab_rect_center = p.Subtract(s.Divide(2.0));
-            var rect = new Rectangle(dab_rect_center, s);
-            using (Brush brush = new SolidBrush(color))
+            var rect = new SD.Rectangle(dab_rect_center, s);
+            using (SD.Brush brush = new SD.SolidBrush(color))
             {
                 this._gfx.FillEllipse(brush, rect);
 
@@ -89,7 +89,7 @@ namespace WinTabPainter
         public void Load(string filename)
         {
             this.dispose_resources();
-            this._bmp = (Bitmap) System.Drawing.Bitmap.FromFile(filename);
+            this._bmp = (SD.Bitmap) System.Drawing.Bitmap.FromFile(filename);
             this._width = this._bmp.Width;
             this._height= this._bmp.Height;
             this._gfx = System.Drawing.Graphics.FromImage(this._bmp);
