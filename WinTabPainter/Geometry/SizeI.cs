0fbc996e6caae4e7e5a1f908d555882d720bad0d
TheSevenPens <TheSevenPens@gmail.com>
2024-06-22 08:51:09 -0700

    SizeI moved to its own class

---
+++ WinTabPainter/Geometry/SizeI.cs
@@ -0,0 +1,29 @@
+﻿
+using SD = System.Drawing;
+
+namespace WinTabPainter.Geometry
+{
+    public struct SizeI
+    {
+        public int Width;
+        public int Height;
+
+        public SizeI(int w, int h)
+        {
+            this.Width = w;
+            this.Height = h;
+        }
+
+        public SizeD Divide(double scale)
+        {
+            return new SizeD(this.Width / scale, this.Height / scale);
+        }
+
+        public SD.Size ToSDSize()
+        {
+            var s = new SD.Size(this.Width, this.Height);
+            return s;
+        }
+    }
+
+}
