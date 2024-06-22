354f1f06aed195b6f0a49e967bd21aff2cb5adcb
TheSevenPens <TheSevenPens@gmail.com>
2024-06-22 09:14:09 -0700

    Moved PointI to its own class

---
+++ WinTabPainter/Geometry/PointI.cs
@@ -0,0 +1,55 @@
+﻿using SD = System.Drawing;
+
+namespace WinTabPainter.Geometry
+{
+    public struct PointI
+    {
+        public int X;
+        public int Y;
+
+        public static PointI Empty
+        {
+            get
+            {
+                return new PointI(0, 0);
+            }
+        }
+
+        public PointI(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+        public PointI Add(int dx, int dy)
+        {
+            return new PointI(this.X + dx, this.Y + dy);
+        }
+        public PointI Subtract(int dx, int dy)
+        {
+            return new PointI(this.X - dx, this.Y - dy);
+        }
+
+        public PointI Subtract(Geometry.SizeI s)
+        {
+            return new PointI(this.X - s.Width, this.Y - s.Height);
+        }
+
+
+        public PointD Divide(double scale)
+        {
+            return new PointD(this.X / scale, this.Y / scale);
+        }
+
+        public SD.Point ToSDPoint()
+        {
+            var p = new SD.Point(this.X, this.Y);
+            return p;
+        }
+
+        public string ToSmallString()
+        {
+            return string.Format("({0}x{1})", this.X, this.Y);
+        }
+    }
+
+}
