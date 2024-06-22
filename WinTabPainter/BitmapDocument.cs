346365e335fdbc70c879e42a1667df6b501f58b9
TheSevenPens <TheSevenPens@gmail.com>
2024-06-22 00:42:21 -0700

    Removed duplicated comments

--- WinTabPainter/BitmapDocument.cs
+++ WinTabPainter/BitmapDocument.cs
@@ -1,15 +1,6 @@
 ﻿using System;
 using SD=System.Drawing;
 
-
-
-// References:
-// https://github.com/DennisWacom/WintabControl/tree/master/WintabControl
-// https://github.com/DennisWacom/InkPlatform/tree/master/WintabDN
-// https://developer-docs.wacom.com/docs/icbt/windows/wintab/wintab-basics/
-// https://www.nuget.org/packages/WacomSolutionPartner.WintabDotNet
-// https://github.com/Wacom-Developer/wacom-device-kit-windows/tree/master/Wintab%20TiltTest
-
 namespace WinTabPainter
 {
     public class BitmapDocument : IDisposable
--- WinTabPainter/EMASmoother.cs
+++ WinTabPainter/EMASmoother.cs
@@ -1,11 +1,4 @@
-﻿// References:
-// https://github.com/DennisWacom/WintabControl/tree/master/WintabControl
-// https://github.com/DennisWacom/InkPlatform/tree/master/WintabDN
-// https://developer-docs.wacom.com/docs/icbt/windows/wintab/wintab-basics/
-// https://www.nuget.org/packages/WacomSolutionPartner.WintabDotNet
-// https://github.com/Wacom-Developer/wacom-device-kit-windows/tree/master/Wintab%20TiltTest
-
-namespace WinTabPainter
+﻿namespace WinTabPainter
 {
     public class EMASmoother
     {
