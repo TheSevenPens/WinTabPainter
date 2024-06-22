317be1b984db8abe6a10b2f04d08fc7d56b160f4
TheSevenPens <TheSevenPens@gmail.com>
2024-06-22 08:54:11 -0700

    Added PointI struct

--- WinTabPainter/Geometry/PointD.cs
+++ WinTabPainter/Geometry/PointD.cs
@@ -20,7 +20,7 @@ namespace WinTabPainter.Geometry
             this.X = x;
             this.Y = y;
         }
-        public PointD Add(int dx, int dy)
+        public PointD Add(double dx, double dy)
         {
             return new PointD(this.X + dx, this.Y + dy);
         }
@@ -43,4 +43,46 @@ namespace WinTabPainter.Geometry
             return string.Format("({0}x{1})", this.X, this.Y);
         }
     }
+
+
+    public struct PointI
+    {
+        public int X;
+        public int Y;
+
+        public static PointI Empty
+        {
+            get
+            {
+                return new PointI(0, 0);
+            }
+        }
+
+        public PointI(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+        public PointI Add(int dx, int dy)
+        {
+            return new PointI(this.X + dx, this.Y + dy);
+        }
+
+        public PointD Divide(double scale)
+        {
+            return new PointD(this.X / scale, this.Y / scale);
+        }
+
+        public SD.Point ToSDPoint()
+        {
+            var p = new SD.Point(this.X, this.Y);
+            return p;
+        }
+
+        public string ToSmallString()
+        {
+            return string.Format("({0}x{1})", this.X, this.Y);
+        }
+    }
+
 }
