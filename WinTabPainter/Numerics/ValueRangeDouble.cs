701a3ab6587561a965399dea9379936990074502
TheSevenPens <TheSevenPens@gmail.com>
2024-06-25 20:49:04 -0700

    Moved classes to separate files

--- WinTabPainter/Numerics/ValueRangeDouble.cs
+++ WinTabPainter/Numerics/ValueRangeDouble.cs
@@ -1,30 +1,5 @@
 ﻿namespace WinTabPainter.Numerics
 {
-    public struct ValueRangeInt
-    {
-
-        public readonly int Lower;
-        public readonly int Upper;
-
-        public ValueRangeInt(int lower, int upper)
-        {
-            this.Lower = lower;
-            this.Upper = upper;
-        }
-
-        public int Width => this.Upper - this.Lower;
-
-        public int Clamp(int value) { return this.ClampRangeInt(value, this.Lower, this.Upper); }
-
-        private int ClampRangeInt(int value, int min, int max)
-        {
-            if (value < min) { value = min; }
-            else if (value > max) { value = max; }
-            else { /* dnothing */}
-            return value;
-        }
-    }
-
     public struct ValueRangeDouble
     {
 
