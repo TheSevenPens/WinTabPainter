a26cf9a242de5f03a8b4993661cc7f842dd68ab2
TheSevenPens <TheSevenPens@gmail.com>
2024-07-02 23:37:31 -0700

    Replaced switches with pattern matching

--- WinTabUtils/PenButtonInfo.cs
+++ WinTabUtils/PenButtonInfo.cs
@@ -13,40 +13,21 @@ public struct PenButtonInfo
         this.Id = (UInt16)((pkt_button & 0x0000FFFF) >> 0);
         UInt16 press_status = (UInt16)((pkt_button & 0xFFFF0000) >> 16);
 
-        if (press_status == 0)
+        this.PressStatus = press_status switch
         {
-            this.PressStatus = PenButtonStatus.NoChange;
-        }
-        else if (press_status == 1)
-        {
-            this.PressStatus = PenButtonStatus.Released;
-        }
-        else if (press_status == 2)
-        {
-            this.PressStatus = PenButtonStatus.Pressed;
-        }
-        else
-        {
-            throw new System.ArgumentOutOfRangeException();
-        }
+            0 => PenButtonStatus.NoChange,
+            1 => PenButtonStatus.Released,
+            2 => PenButtonStatus.Pressed,
+            _ => throw new System.ArgumentOutOfRangeException()
+        };
 
-        if (this.Id == 0)
+        this.Type = this.Id switch
         {
-            this.Type = PenButtonType.Tip;
-        }
-        else if (this.Id == 1)
-        {
-            this.Type = PenButtonType.LowerButton;
-        }
-        else if (this.Id == 2)
-        {
-            this.Type = PenButtonType.UpperButton;
-        }
-        else
-        {
-            throw new System.ArgumentOutOfRangeException();
-        }
-
+            0 => PenButtonType.Tip,
+            1 => PenButtonType.LowerButton,
+            2 => PenButtonType.UpperButton,
+            _ => throw new System.ArgumentOutOfRangeException()
+        };
     }
 
     public override string ToString()
