7276ab93e1fdf99c93c092f893a574fedca7b718
TheSevenPens <TheSevenPens@gmail.com>
2025-03-19 00:42:26 -0700

    reorderd paintsettings members

--- WinTabPainter/Painting/PaintSettings.cs
+++ WinTabPainter/Painting/PaintSettings.cs
@@ -2,38 +2,38 @@
 
 public class PaintSettings
 {
-    // brush settings
-    private int brushWidth = 25;
+    // statics
     public static readonly Numerics.OrderedRange SYS_BRUSHSIZE_RANGE = new Numerics.OrderedRange(1, 100);
+    public static readonly Numerics.OrderedRangeD SYS_SMOOTHING_RANGE = new Numerics.OrderedRangeD(0, 1);
+    public static readonly Numerics.OrderedRangeD SYS_SMOOTHING_RANGE_LIMITED = new Numerics.OrderedRangeD(0.0, 0.995);
+
+    // Brush Width
+    private int brushWidth = 25;
+    public int BrushWidth
+    {
+        get => brushWidth;
+        set => brushWidth = PaintSettings.SYS_BRUSHSIZE_RANGE.Clamp(value);
+    }
 
     // pressure settings
     public Numerics.SimpleCurve pressure_curve = new Numerics.SimpleCurve();
     public int PressureQuantizeLevels = -1;
 
-    // noise
+    // Position Noise
     public int PosXNoise;
     public int PosYNoise;
 
-
-    // smoothing settings
+    // Smoothing 
     public readonly Numerics.EMAPositionSmoother PositionSmoother;
     public readonly Numerics.EMASmoother PressureSmoother;
 
-    public static readonly Numerics.OrderedRangeD SYS_SMOOTHING_RANGE = new Numerics.OrderedRangeD(0, 1);
-    public static readonly Numerics.OrderedRangeD SYS_SMOOTHING_RANGE_LIMITED = new Numerics.OrderedRangeD(0.0, 0.995);
 
-
-    public int BrushWidth { 
-        get => brushWidth; 
-        set => brushWidth = PaintSettings.SYS_BRUSHSIZE_RANGE.Clamp(value); }
-
+    // Anti-Aliasing
     public bool AntiAliasing = true;
+
     public PaintSettings()
     {
         this.PositionSmoother = new Numerics.EMAPositionSmoother(0);
         this.PressureSmoother = new Numerics.EMASmoother(0);
     }
-
-
-
 }
