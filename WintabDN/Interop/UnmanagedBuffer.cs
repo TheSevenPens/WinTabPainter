0f7df7ffb26314274407b501fb67a321a863f926
TheSevenPens <TheSevenPens@gmail.com>
2025-03-20 08:21:50 -0700

    Renaming

--- WintabDN/Interop/UnmanagedBuffer.cs
+++ WintabDN/Interop/UnmanagedBuffer.cs
@@ -109,81 +109,8 @@ public class UnmanagedBuffer : IDisposable
         this.assert_type(typeof(T));
         var value = (T)System.Runtime.InteropServices.Marshal.PtrToStructure(this.Pointer, typeof(T));
         return value;
-
     }
 
-
-
-    /// <summary>
-    /// Marshal unmanaged data packets into managed WintabPacket data.
-    /// </summary>
-    /// <param name="num_pkts">number of packets to marshal</param>
-    /// <param name="buf_ptr">pointer to unmanaged heap memory containing data packets</param>
-    /// <returns></returns>
-
-
-    public WintabDN.Structs.WintabPacket[] MarshalDataPacketsFromBuffer(UInt32 num_pkts)
-    {
-        if (num_pkts == 0 || this.Pointer == IntPtr.Zero)
-        {
-            return null;
-        }
-
-        var packets = new WintabDN.Structs.WintabPacket[num_pkts];
-
-        var temp_packet = new WintabDN.Structs.WintabPacket();
-        int pkt_size = System.Runtime.InteropServices.Marshal.SizeOf(temp_packet);
-
-        for (int i = 0; i < num_pkts; i++)
-        {
-            packets[i] = (WintabDN.Structs.WintabPacket)System.Runtime.InteropServices.Marshal.PtrToStructure(IntPtr.Add(this.Pointer, i * pkt_size), typeof(WintabDN.Structs.WintabPacket));
-        }
-        return packets;
-    }
-
-
-    /// <summary>
-    /// Marshal unmanaged Extension data packets into managed WintabPacketExt data.
-    /// </summary>
-    /// <param name="num_pkts">number of packets to marshal</param>
-    /// <param name="buf_ptr">pointer to unmanaged heap memory containing data packets</param>
-    /// <returns></returns>
-    public WintabDN.Structs.WintabPacketExt[] MarshalDataExtPacketsFromBuffer(UInt32 num_pkts)
-    {
-        var packets = new WintabDN.Structs.WintabPacketExt[num_pkts];
-
-        if (num_pkts == 0 || this.Pointer == IntPtr.Zero)
-        {
-            return null;
-        }
-
-        // Marshal each WintabPacketExt in the array separately.
-        // This is "necessary" because none of the other ways I tried to marshal
-        // seemed to work.  It's ugly, but it works.
-
-        var temp_pkt_ext = new WintabDN.Structs.WintabPacketExt();
-        int pkt_size = System.Runtime.InteropServices.Marshal.SizeOf(temp_pkt_ext);
-        var bytes = new Byte[num_pkts * pkt_size];
-        System.Runtime.InteropServices.Marshal.Copy(this.Pointer, bytes, 0, (int)num_pkts * pkt_size);
-
-        var temp_bytes = new Byte[pkt_size];
-
-        for (int pkt_i = 0; pkt_i < num_pkts; pkt_i++)
-        {
-            for (int i = 0; i < pkt_size; i++)
-            {
-                temp_bytes[i] = bytes[(pkt_i * pkt_size) + i];
-            }
-
-            using (var tmpbuf = WintabDN.Interop.UnmanagedBuffer.CreateForObject<WintabDN.Structs.WintabPacketExt>())
-            {
-                System.Runtime.InteropServices.Marshal.Copy(temp_bytes, 0, tmpbuf.Pointer, pkt_size);
-                packets[pkt_i] = tmpbuf.MarshallFromBuffer<WintabDN.Structs.WintabPacketExt>();
-            }
-        }
-
-        return packets;
-    }
 
     public void Dispose()
     {
@@ -249,5 +176,87 @@ public class UnmanagedBuffer : IDisposable
         var encoding = System.Text.Encoding.UTF8;
         string value = encoding.GetString(bytes);
         return value;
+    }
+
+
+    /// <summary>
+    /// Marshal unmanaged data packets into managed WintabPacket data.
+    /// </summary>
+    /// <param name="num_pkts">number of packets to marshal</param>
+    /// <param name="buf_ptr">pointer to unmanaged heap memory containing data packets</param>
+    /// <returns></returns>
+
+
+    public WintabDN.Structs.WintabPacket[] MarshalDataPacketsFromBuffer(UInt32 num_pkts)
+    {
+        if (num_pkts == 0)
+        {
+            return null;
+        }
+
+        if (this.Pointer == IntPtr.Zero)
+        {
+            return null;
+        }
+
+        var packets = new WintabDN.Structs.WintabPacket[num_pkts];
+
+        var temp_packet = new WintabDN.Structs.WintabPacket();
+        int pkt_size = System.Runtime.InteropServices.Marshal.SizeOf(temp_packet);
+
+        for (int i = 0; i < num_pkts; i++)
+        {
+            packets[i] = (WintabDN.Structs.WintabPacket)System.Runtime.InteropServices.Marshal.PtrToStructure(IntPtr.Add(this.Pointer, i * pkt_size), typeof(WintabDN.Structs.WintabPacket));
+        }
+        return packets;
+    }
+
+
+    /// <summary>
+    /// Marshal unmanaged Extension data packets into managed WintabPacketExt data.
+    /// </summary>
+    /// <param name="num_pkts">number of packets to marshal</param>
+    /// <param name="buf_ptr">pointer to unmanaged heap memory containing data packets</param>
+    /// <returns></returns>
+    public WintabDN.Structs.WintabPacketExt[] MarshalDataExtPacketsFromBuffer(UInt32 num_pkts)
+    {
+        var packets = new WintabDN.Structs.WintabPacketExt[num_pkts];
+
+        if (num_pkts == 0)
+        {
+            return null;
+        }
+
+        if (this.Pointer == IntPtr.Zero)
+        {
+            return null;
+        }
+
+        // Marshal each WintabPacketExt in the array separately.
+        // This is "necessary" because none of the other ways I tried to marshal
+        // seemed to work.  It's ugly, but it works.
+
+        var temp_pkt_ext = new WintabDN.Structs.WintabPacketExt();
+        int pkt_size = System.Runtime.InteropServices.Marshal.SizeOf(temp_pkt_ext);
+        var bytes = new Byte[num_pkts * pkt_size];
+        System.Runtime.InteropServices.Marshal.Copy(this.Pointer, bytes, 0, (int)num_pkts * pkt_size);
+
+        var temp_bytes = new Byte[pkt_size];
+
+        for (int pkt_i = 0; pkt_i < num_pkts; pkt_i++)
+        {
+            for (int i = 0; i < pkt_size; i++)
+            {
+                temp_bytes[i] = bytes[(pkt_i * pkt_size) + i];
+            }
+
+            using (var tmpbuf = WintabDN.Interop.UnmanagedBuffer.CreateForObject<WintabDN.Structs.WintabPacketExt>())
+            {
+                System.Runtime.InteropServices.Marshal.Copy(temp_bytes, 0, tmpbuf.Pointer, pkt_size);
+                packets[pkt_i] = tmpbuf.MarshallFromBuffer<WintabDN.Structs.WintabPacketExt>();
+            }
+        }
+
+        return packets;
     }
 }
