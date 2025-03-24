94f63a04dedcd905e945759d9d577746e8cce475
TheSevenPens <TheSevenPens@gmail.com>
2025-03-24 11:29:32 -0700

    Added moving average for normalized pressure

--- WinTabPressureTester/FormPressureTester.Designer.cs
+++ WinTabPressureTester/FormPressureTester.Designer.cs
@@ -31,7 +31,7 @@
             label_pressure_raw = new Label();
             label_raw_pressure_txt = new Label();
             label1 = new Label();
-            label_pressure_level = new Label();
+            label_normalized_pressure = new Label();
             label2 = new Label();
             label_or_azimuth = new Label();
             label4 = new Label();
@@ -41,6 +41,8 @@
             label_tilty = new Label();
             label6 = new Label();
             label_tiltx = new Label();
+            label5 = new Label();
+            label_normalizedpressure_ma = new Label();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
@@ -48,7 +50,7 @@
             // 
             label_pressure_raw.AutoSize = true;
             label_pressure_raw.Font = new Font("Consolas", 26.1F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label_pressure_raw.Location = new Point(62, 64);
+            label_pressure_raw.Location = new Point(426, 23);
             label_pressure_raw.Name = "label_pressure_raw";
             label_pressure_raw.Size = new Size(283, 102);
             label_pressure_raw.TabIndex = 0;
@@ -57,7 +59,7 @@
             // label_raw_pressure_txt
             // 
             label_raw_pressure_txt.AutoSize = true;
-            label_raw_pressure_txt.Location = new Point(53, 23);
+            label_raw_pressure_txt.Location = new Point(53, 64);
             label_raw_pressure_txt.Name = "label_raw_pressure_txt";
             label_raw_pressure_txt.Size = new Size(193, 41);
             label_raw_pressure_txt.TabIndex = 1;
@@ -66,26 +68,26 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(378, 23);
+            label1.Location = new Point(62, 194);
             label1.Name = "label1";
-            label1.Size = new Size(205, 41);
+            label1.Size = new Size(286, 41);
             label1.TabIndex = 3;
-            label1.Text = "Pressure Level";
+            label1.Text = "Pressure normalized";
             // 
-            // label_pressure_level
+            // label_normalized_pressure
             // 
-            label_pressure_level.AutoSize = true;
-            label_pressure_level.Font = new Font("Consolas", 26.1F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label_pressure_level.Location = new Point(387, 64);
-            label_pressure_level.Name = "label_pressure_level";
-            label_pressure_level.Size = new Size(379, 102);
-            label_pressure_level.TabIndex = 2;
-            label_pressure_level.Text = "-------";
+            label_normalized_pressure.AutoSize = true;
+            label_normalized_pressure.Font = new Font("Consolas", 26.1F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label_normalized_pressure.Location = new Point(426, 194);
+            label_normalized_pressure.Name = "label_normalized_pressure";
+            label_normalized_pressure.Size = new Size(379, 102);
+            label_normalized_pressure.TabIndex = 2;
+            label_normalized_pressure.Text = "-------";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(448, 277);
+            label2.Location = new Point(54, 707);
             label2.Name = "label2";
             label2.Size = new Size(282, 41);
             label2.TabIndex = 7;
@@ -95,7 +97,7 @@
             // 
             label_or_azimuth.AutoSize = true;
             label_or_azimuth.Font = new Font("Consolas", 26.1F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label_or_azimuth.Location = new Point(457, 318);
+            label_or_azimuth.Location = new Point(426, 707);
             label_or_azimuth.Name = "label_or_azimuth";
             label_or_azimuth.Size = new Size(187, 102);
             label_or_azimuth.TabIndex = 6;
@@ -104,7 +106,7 @@
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(62, 277);
+            label4.Location = new Point(54, 536);
             label4.Name = "label4";
             label4.Size = new Size(276, 41);
             label4.TabIndex = 5;
@@ -114,7 +116,7 @@
             // 
             label_or_altitude.AutoSize = true;
             label_or_altitude.Font = new Font("Consolas", 26.1F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label_or_altitude.Location = new Point(71, 318);
+            label_or_altitude.Location = new Point(426, 536);
             label_or_altitude.Name = "label_or_altitude";
             label_or_altitude.Size = new Size(187, 102);
             label_or_altitude.TabIndex = 4;
@@ -131,7 +133,7 @@
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(62, 648);
+            label3.Location = new Point(54, 1049);
             label3.Name = "label3";
             label3.Size = new Size(233, 41);
             label3.TabIndex = 12;
@@ -141,7 +143,7 @@
             // 
             label_tilty.AutoSize = true;
             label_tilty.Font = new Font("Consolas", 26.1F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label_tilty.Location = new Point(71, 689);
+            label_tilty.Location = new Point(426, 1049);
             label_tilty.Name = "label_tilty";
             label_tilty.Size = new Size(379, 102);
             label_tilty.TabIndex = 11;
@@ -150,7 +152,7 @@
             // label6
             // 
             label6.AutoSize = true;
-            label6.Location = new Point(62, 492);
+            label6.Location = new Point(63, 878);
             label6.Name = "label6";
             label6.Size = new Size(232, 41);
             label6.TabIndex = 10;
@@ -160,17 +162,38 @@
             // 
             label_tiltx.AutoSize = true;
             label_tiltx.Font = new Font("Consolas", 26.1F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label_tiltx.Location = new Point(71, 533);
+            label_tiltx.Location = new Point(426, 878);
             label_tiltx.Name = "label_tiltx";
             label_tiltx.Size = new Size(379, 102);
             label_tiltx.TabIndex = 9;
             label_tiltx.Text = "-------";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(53, 365);
+            label5.Name = "label5";
+            label5.Size = new Size(358, 41);
+            label5.TabIndex = 14;
+            label5.Text = "Pressure normalized (MA)";
+            // 
+            // label_normalizedpressure_ma
+            // 
+            label_normalizedpressure_ma.AutoSize = true;
+            label_normalizedpressure_ma.Font = new Font("Consolas", 26.1F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label_normalizedpressure_ma.Location = new Point(426, 365);
+            label_normalizedpressure_ma.Name = "label_normalizedpressure_ma";
+            label_normalizedpressure_ma.Size = new Size(379, 102);
+            label_normalizedpressure_ma.TabIndex = 13;
+            label_normalizedpressure_ma.Text = "-------";
             // 
             // FormPressureTester
             // 
             AutoScaleDimensions = new SizeF(17F, 41F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(2023, 1388);
+            Controls.Add(label5);
+            Controls.Add(label_normalizedpressure_ma);
             Controls.Add(label3);
             Controls.Add(label_tilty);
             Controls.Add(label6);
@@ -181,7 +204,7 @@
             Controls.Add(label4);
             Controls.Add(label_or_altitude);
             Controls.Add(label1);
-            Controls.Add(label_pressure_level);
+            Controls.Add(label_normalized_pressure);
             Controls.Add(label_raw_pressure_txt);
             Controls.Add(label_pressure_raw);
             Name = "FormPressureTester";
@@ -198,7 +221,7 @@
         private Label label_pressure_raw;
         private Label label_raw_pressure_txt;
         private Label label1;
-        private Label label_pressure_level;
+        private Label label_normalized_pressure;
         private Label label2;
         private Label label_or_azimuth;
         private Label label4;
@@ -208,5 +231,7 @@
         private Label label_tilty;
         private Label label6;
         private Label label_tiltx;
+        private Label label5;
+        private Label label_normalizedpressure_ma;
     }
 }
--- WinTabPressureTester/FormPressureTester.cs
+++ WinTabPressureTester/FormPressureTester.cs
@@ -3,10 +3,12 @@ namespace WinTabPressureTester
     public partial class FormPressureTester : Form
     {
         WinTabUtils.TabletSession wintabsession;
+        WinTabUtils.Numerics.MovingAverage ma;
         public FormPressureTester()
         {
             InitializeComponent();
             this.wintabsession = new WinTabUtils.TabletSession();
+            this.ma = new WinTabUtils.Numerics.MovingAverage(600);
         }
 
         Graphics gfx_picbox1;
@@ -14,7 +16,8 @@ namespace WinTabPressureTester
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            this.wintabsession.PacketHandler = PacketHandler;
+            this.wintabsession.PacketHandler = this.PacketHandler;
+            this.wintabsession.ButtonChangedHandler = this.ButtonChangeHandler;
             this.wintabsession.Open(WinTabUtils.TabletContextType.System);
             this.pictureBox1.Image = new System.Drawing.Bitmap(this.pictureBox1.Width, this.pictureBox1.Height);
             this.gfx_picbox1 = System.Drawing.Graphics.FromImage(this.pictureBox1.Image);
@@ -35,18 +38,31 @@ namespace WinTabPressureTester
             uint raw_pressure = wintab_pkt.pkNormalPressure;
             double normalized_raw_pressure = (raw_pressure / (1.0 * this.wintabsession.TabletInfo.MaxPressure));
 
-            string str_pressure = string.Format("{0:00.000}%", normalized_raw_pressure* 100.0);
+            string str_pressure = string.Format("{0:00.000}%", normalized_raw_pressure * 100.0);
 
-            (double TiltX, double TiltY) = WinTabUtils.Trigonometry.Angles.AzimuthAndAltudeToTiltDeg(wintab_pkt.pkOrientation.orAzimuth/10.0, wintab_pkt.pkOrientation.orAltitude/ 10.0);
+            (double TiltX, double TiltY) = WinTabUtils.Trigonometry.Angles.AzimuthAndAltudeToTiltDeg(wintab_pkt.pkOrientation.orAzimuth / 10.0, wintab_pkt.pkOrientation.orAltitude / 10.0);
 
             this.label_pressure_raw.Text = wintab_pkt.pkNormalPressure.ToString();
-            this.label_pressure_level.Text = str_pressure.ToString();
+            this.label_normalized_pressure.Text = str_pressure.ToString();
 
             this.label_or_altitude.Text = (wintab_pkt.pkOrientation.orAltitude / 10.0).ToString() + "°";
             this.label_or_azimuth.Text = (wintab_pkt.pkOrientation.orAzimuth / 10.0).ToString() + "°";
             this.label_tiltx.Text = string.Format("{0:00.000}°", TiltX);
             this.label_tilty.Text = string.Format("{0:00.000}°", TiltY);
 
+
+
+            this.ma.AddSample(normalized_raw_pressure);
+            this.label_normalizedpressure_ma.Text = string.Format("{0:00.00}%", ma.GetAverage() * 100.0);
+
+        }
+
+        private void ButtonChangeHandler(WintabDN.Structs.WintabPacket wintab_pkt, WinTabUtils.PenButtonPressChange buttonchange)
+        {
+            if (buttonchange.Change == WinTabUtils.PenButtonPressChangeType.Released)
+            {
+                this.ma.Clear();
+            }
         }
     }
 }
