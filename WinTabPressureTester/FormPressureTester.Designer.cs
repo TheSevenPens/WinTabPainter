fa98e99c616c033b262cdee16252e0b1b205ef2b
TheSevenPens <98374109+TheSevenPens@users.noreply.github.com>
2025-04-03 02:49:06 -0700

    Added scale measurements

--- WinTabPressureTester/FormPressureTester.Designer.cs
+++ WinTabPressureTester/FormPressureTester.Designer.cs
@@ -43,6 +43,12 @@
             label_tiltx = new Label();
             label5 = new Label();
             label_normalizedpressure_ma = new Label();
+            textBox_log = new TextBox();
+            button_start = new Button();
+            button_stop = new Button();
+            label_force = new Label();
+            button1 = new Button();
+            button2 = new Button();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
@@ -50,27 +56,30 @@
             // 
             label_pressure_raw.AutoSize = true;
             label_pressure_raw.Font = new Font("Consolas", 26.1F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label_pressure_raw.Location = new Point(426, 23);
+            label_pressure_raw.Location = new Point(326, 18);
+            label_pressure_raw.Margin = new Padding(2, 0, 2, 0);
             label_pressure_raw.Name = "label_pressure_raw";
-            label_pressure_raw.Size = new Size(283, 102);
+            label_pressure_raw.Size = new Size(225, 82);
             label_pressure_raw.TabIndex = 0;
             label_pressure_raw.Text = "-----";
             // 
             // label_raw_pressure_txt
             // 
             label_raw_pressure_txt.AutoSize = true;
-            label_raw_pressure_txt.Location = new Point(53, 64);
+            label_raw_pressure_txt.Location = new Point(41, 50);
+            label_raw_pressure_txt.Margin = new Padding(2, 0, 2, 0);
             label_raw_pressure_txt.Name = "label_raw_pressure_txt";
-            label_raw_pressure_txt.Size = new Size(193, 41);
+            label_raw_pressure_txt.Size = new Size(153, 32);
             label_raw_pressure_txt.TabIndex = 1;
             label_raw_pressure_txt.Text = "Raw Pressure";
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(62, 194);
+            label1.Location = new Point(47, 151);
+            label1.Margin = new Padding(2, 0, 2, 0);
             label1.Name = "label1";
-            label1.Size = new Size(286, 41);
+            label1.Size = new Size(229, 32);
             label1.TabIndex = 3;
             label1.Text = "Pressure normalized";
             // 
@@ -78,18 +87,20 @@
             // 
             label_normalized_pressure.AutoSize = true;
             label_normalized_pressure.Font = new Font("Consolas", 26.1F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label_normalized_pressure.Location = new Point(426, 194);
+            label_normalized_pressure.Location = new Point(326, 151);
+            label_normalized_pressure.Margin = new Padding(2, 0, 2, 0);
             label_normalized_pressure.Name = "label_normalized_pressure";
-            label_normalized_pressure.Size = new Size(379, 102);
+            label_normalized_pressure.Size = new Size(301, 82);
             label_normalized_pressure.TabIndex = 2;
             label_normalized_pressure.Text = "-------";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(54, 707);
+            label2.Location = new Point(41, 552);
+            label2.Margin = new Padding(2, 0, 2, 0);
             label2.Name = "label2";
-            label2.Size = new Size(282, 41);
+            label2.Size = new Size(228, 32);
             label2.TabIndex = 7;
             label2.Text = "Orientation azimuth";
             // 
@@ -97,18 +108,20 @@
             // 
             label_or_azimuth.AutoSize = true;
             label_or_azimuth.Font = new Font("Consolas", 26.1F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label_or_azimuth.Location = new Point(426, 707);
+            label_or_azimuth.Location = new Point(326, 552);
+            label_or_azimuth.Margin = new Padding(2, 0, 2, 0);
             label_or_azimuth.Name = "label_or_azimuth";
-            label_or_azimuth.Size = new Size(187, 102);
+            label_or_azimuth.Size = new Size(149, 82);
             label_or_azimuth.TabIndex = 6;
             label_or_azimuth.Text = "---";
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(54, 536);
+            label4.Location = new Point(41, 418);
+            label4.Margin = new Padding(2, 0, 2, 0);
             label4.Name = "label4";
-            label4.Size = new Size(276, 41);
+            label4.Size = new Size(223, 32);
             label4.TabIndex = 5;
             label4.Text = "Orientation altitude";
             // 
@@ -116,26 +129,29 @@
             // 
             label_or_altitude.AutoSize = true;
             label_or_altitude.Font = new Font("Consolas", 26.1F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label_or_altitude.Location = new Point(426, 536);
+            label_or_altitude.Location = new Point(326, 418);
+            label_or_altitude.Margin = new Padding(2, 0, 2, 0);
             label_or_altitude.Name = "label_or_altitude";
-            label_or_altitude.Size = new Size(187, 102);
+            label_or_altitude.Size = new Size(149, 82);
             label_or_altitude.TabIndex = 4;
             label_or_altitude.Text = "---";
             // 
             // pictureBox1
             // 
-            pictureBox1.Location = new Point(1098, 64);
+            pictureBox1.Location = new Point(840, 50);
+            pictureBox1.Margin = new Padding(2);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(600, 500);
+            pictureBox1.Size = new Size(459, 390);
             pictureBox1.TabIndex = 8;
             pictureBox1.TabStop = false;
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(54, 1049);
+            label3.Location = new Point(41, 819);
+            label3.Margin = new Padding(2, 0, 2, 0);
             label3.Name = "label3";
-            label3.Size = new Size(233, 41);
+            label3.Size = new Size(189, 32);
             label3.TabIndex = 12;
             label3.Text = "Orientation tilt y";
             // 
@@ -143,18 +159,20 @@
             // 
             label_tilty.AutoSize = true;
             label_tilty.Font = new Font("Consolas", 26.1F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label_tilty.Location = new Point(426, 1049);
+            label_tilty.Location = new Point(326, 819);
+            label_tilty.Margin = new Padding(2, 0, 2, 0);
             label_tilty.Name = "label_tilty";
-            label_tilty.Size = new Size(379, 102);
+            label_tilty.Size = new Size(301, 82);
             label_tilty.TabIndex = 11;
             label_tilty.Text = "-------";
             // 
             // label6
             // 
             label6.AutoSize = true;
-            label6.Location = new Point(63, 878);
+            label6.Location = new Point(48, 685);
+            label6.Margin = new Padding(2, 0, 2, 0);
             label6.Name = "label6";
-            label6.Size = new Size(232, 41);
+            label6.Size = new Size(188, 32);
             label6.TabIndex = 10;
             label6.Text = "Orientation tilt x";
             // 
@@ -162,18 +180,20 @@
             // 
             label_tiltx.AutoSize = true;
             label_tiltx.Font = new Font("Consolas", 26.1F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label_tiltx.Location = new Point(426, 878);
+            label_tiltx.Location = new Point(326, 685);
+            label_tiltx.Margin = new Padding(2, 0, 2, 0);
             label_tiltx.Name = "label_tiltx";
-            label_tiltx.Size = new Size(379, 102);
+            label_tiltx.Size = new Size(301, 82);
             label_tiltx.TabIndex = 9;
             label_tiltx.Text = "-------";
             // 
             // label5
             // 
             label5.AutoSize = true;
-            label5.Location = new Point(53, 365);
+            label5.Location = new Point(41, 285);
+            label5.Margin = new Padding(2, 0, 2, 0);
             label5.Name = "label5";
-            label5.Size = new Size(358, 41);
+            label5.Size = new Size(287, 32);
             label5.TabIndex = 14;
             label5.Text = "Pressure normalized (MA)";
             // 
@@ -181,17 +201,82 @@
             // 
             label_normalizedpressure_ma.AutoSize = true;
             label_normalizedpressure_ma.Font = new Font("Consolas", 26.1F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label_normalizedpressure_ma.Location = new Point(426, 365);
+            label_normalizedpressure_ma.Location = new Point(326, 285);
+            label_normalizedpressure_ma.Margin = new Padding(2, 0, 2, 0);
             label_normalizedpressure_ma.Name = "label_normalizedpressure_ma";
-            label_normalizedpressure_ma.Size = new Size(379, 102);
+            label_normalizedpressure_ma.Size = new Size(301, 82);
             label_normalizedpressure_ma.TabIndex = 13;
             label_normalizedpressure_ma.Text = "-------";
             // 
+            // textBox_log
+            // 
+            textBox_log.Location = new Point(1435, 99);
+            textBox_log.Multiline = true;
+            textBox_log.Name = "textBox_log";
+            textBox_log.Size = new Size(684, 902);
+            textBox_log.TabIndex = 15;
+            // 
+            // button_start
+            // 
+            button_start.Location = new Point(908, 576);
+            button_start.Name = "button_start";
+            button_start.Size = new Size(150, 46);
+            button_start.TabIndex = 16;
+            button_start.Text = "start";
+            button_start.UseVisualStyleBackColor = true;
+            button_start.Click += button_start_Click;
+            // 
+            // button_stop
+            // 
+            button_stop.Location = new Point(908, 671);
+            button_stop.Name = "button_stop";
+            button_stop.Size = new Size(150, 46);
+            button_stop.TabIndex = 17;
+            button_stop.Text = "stop";
+            button_stop.UseVisualStyleBackColor = true;
+            button_stop.Click += button_stop_Click;
+            // 
+            // label_force
+            // 
+            label_force.AutoSize = true;
+            label_force.Font = new Font("Consolas", 26.1F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label_force.Location = new Point(908, 462);
+            label_force.Margin = new Padding(2, 0, 2, 0);
+            label_force.Name = "label_force";
+            label_force.Size = new Size(149, 82);
+            label_force.TabIndex = 18;
+            label_force.Text = "---";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(908, 762);
+            button1.Name = "button1";
+            button1.Size = new Size(150, 46);
+            button1.TabIndex = 19;
+            button1.Text = "&record";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(908, 851);
+            button2.Name = "button2";
+            button2.Size = new Size(150, 46);
+            button2.TabIndex = 20;
+            button2.Text = "copy";
+            button2.UseVisualStyleBackColor = true;
+            // 
             // FormPressureTester
             // 
-            AutoScaleDimensions = new SizeF(17F, 41F);
+            AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(2023, 1388);
+            ClientSize = new Size(2201, 1063);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label_force);
+            Controls.Add(button_stop);
+            Controls.Add(button_start);
+            Controls.Add(textBox_log);
             Controls.Add(label5);
             Controls.Add(label_normalizedpressure_ma);
             Controls.Add(label3);
@@ -207,10 +292,14 @@
             Controls.Add(label_normalized_pressure);
             Controls.Add(label_raw_pressure_txt);
             Controls.Add(label_pressure_raw);
+            Margin = new Padding(2);
             Name = "FormPressureTester";
             Text = "WinTabPressureTester";
+            FormClosing += FormPressureTester_FormClosing;
             FormClosed += Form1_FormClosed;
             Load += Form1_Load;
+            KeyDown += FormPressureTester_KeyDown;
+            KeyPress += FormPressureTester_KeyPress;
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
             PerformLayout();
@@ -233,5 +322,11 @@
         private Label label_tiltx;
         private Label label5;
         private Label label_normalizedpressure_ma;
+        private TextBox textBox_log;
+        private Button button_start;
+        private Button button_stop;
+        private Label label_force;
+        private Button button1;
+        private Button button2;
     }
 }
--- WinTabPressureTester/FormPressureTester.cs
+++ WinTabPressureTester/FormPressureTester.cs
@@ -1,9 +1,22 @@
+using System.Diagnostics;
+using System.IO.Ports;
+
 namespace WinTabPressureTester
 {
     public partial class FormPressureTester : Form
     {
         WinTabUtils.TabletSession wintabsession;
         WinTabUtils.Numerics.MovingAverage ma;
+
+
+        private System.IO.Ports.SerialPort serialPort;
+        private CancellationTokenSource cts;
+        private bool isReading = false;
+
+        int num_measuremnts;
+        Stopwatch stopwatch;
+        double physi_force;
+        double log_force;
 
         int px = 0;
 
@@ -12,6 +25,9 @@ namespace WinTabPressureTester
             InitializeComponent();
             this.wintabsession = new WinTabUtils.TabletSession();
             this.ma = new WinTabUtils.Numerics.MovingAverage(600);
+
+            serialPort = new SerialPort("COM6");
+            cts = new CancellationTokenSource();
         }
 
         Graphics gfx_picbox1;
@@ -43,6 +59,7 @@ namespace WinTabPressureTester
             double normalized_raw_pressure = (raw_pressure / (1.0 * this.wintabsession.TabletInfo.MaxPressure));
 
             string str_pressure = string.Format("{0:00.000}%", normalized_raw_pressure * 100.0);
+            this.log_force = normalized_raw_pressure;
 
             (double TiltX, double TiltY) = WinTabUtils.Trigonometry.Angles.AzimuthAndAltudeToTiltDeg(wintab_pkt.pkOrientation.orAzimuth / 10.0, wintab_pkt.pkOrientation.orAltitude / 10.0);
 
@@ -59,10 +76,10 @@ namespace WinTabPressureTester
             this.ma.AddSample(normalized_raw_pressure);
             this.label_normalizedpressure_ma.Text = string.Format("{0:00.00}%", ma.GetAverage() * 100.0);
 
-            int py = this.pictureBox1.Height-10 - (int)(4* 100 * normalized_raw_pressure);
+            int py = this.pictureBox1.Height - 10 - (int)(4 * 100 * normalized_raw_pressure);
             this.gfx_picbox1.DrawLine(this.np_pen_black,
                 new WinTabUtils.Geometry.Point(px, py),
-                new WinTabUtils.Geometry.Point(px, py+1) );
+                new WinTabUtils.Geometry.Point(px, py + 1));
 
             this.px = this.px + 1;
             if (this.px > this.pictureBox1.Width)
@@ -80,6 +97,151 @@ namespace WinTabPressureTester
             {
                 this.ma.Clear();
             }
+        }
+
+        private async void button_start_Click(object sender, EventArgs e)
+        {
+            if (!isReading)
+            {
+                try
+                {
+                    num_measuremnts = 0;
+                    this.stopwatch = Stopwatch.StartNew();
+
+                    if (!serialPort.IsOpen)
+                    {
+                        serialPort.Open();
+                    }
+
+                    isReading = true;
+                    await ReadSerialPortAsync(cts.Token);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error: {ex.Message}");
+                }
+                finally
+                {
+                    if (serialPort.IsOpen)
+                    {
+                        serialPort.Close();
+                    }
+                    isReading = false;
+                }
+            }
+        }
+
+        public string TrimLastCharIf(string s, char c)
+        {
+            if (s == null) { return s; }
+            if (s.Length == 0) { return s; }
+
+            char lastchar = s[^1];
+            if (c == lastchar)
+            {
+                return s.Substring(0, s.Length - 1);
+            }
+            else
+            {
+                return s;
+            }
+
+        }
+        private async Task ReadSerialPortAsync(CancellationToken cancellationToken)
+        {
+            try
+            {
+                while (!cancellationToken.IsCancellationRequested)
+                {
+                    if (serialPort.BytesToRead > 0)
+                    {
+                        string line = await Task.Run(() => serialPort.ReadLine());
+                        this.num_measuremnts++;
+
+                        // Update UI (e.g., append to a TextBox)
+                        if (line == null) { continue; }
+                        line = line.Trim();
+
+                        if (line.Length == 0) { continue; }
+
+                        var tokens = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                        if (tokens.Length == 0) { continue; }
+
+                        string str_nm = this.num_measuremnts.ToString();
+                        string str_mps = (this.num_measuremnts / this.stopwatch.Elapsed.TotalSeconds).ToString();
+                        string str_force = tokens[^1];
+                        str_force = this.TrimLastCharIf(str_force, 'M');
+                        str_force = this.TrimLastCharIf(str_force, 'g');
+
+                         physi_force = double.Parse(str_force);
+
+                        str_force += " gf";
+
+                        if (InvokeRequired)
+                        {
+                            //Invoke(new Action(() => this.textBox_Log.AppendText(line + Environment.NewLine)));
+                            //Invoke(new Action(() => this.label_mps.Text = str_mps));
+                            //Invoke(new Action(() => this.label_num_measuremnts.Text = str_force));
+                        }
+                        else
+                        {
+                            //this.textBox_Log.AppendText(line + Environment.NewLine);
+                            //this.label_mps.Text = str_mps;
+                            //this.label_num_measuremnts.Text = str_nm;
+                            this.label_force.Text = str_force;
+                        }
+
+                    }
+                    // Small delay to prevent CPU overuse
+                    await Task.Delay(10, cancellationToken);
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                // Cancellation was requested
+                this.textBox_log.AppendText("Reading cancelled" + Environment.NewLine);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Serial port error: {ex.Message}");
+            }
+        }
+
+        private void button_stop_Click(object sender, EventArgs e)
+        {
+            // Cancel the reading operation
+            cts.Cancel();
+            // Create new CancellationTokenSource for next operation
+            cts = new CancellationTokenSource();
+        }
+
+        private void FormPressureTester_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+            cts.Cancel();
+            if (serialPort != null && serialPort.IsOpen)
+            {
+                serialPort.Close();
+                serialPort.Dispose();
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string phys = this.physi_force.ToString();
+            string log = this.log_force.ToString();
+            this.textBox_log.AppendText("[ " + phys + " , " + log  + " ], \r\n");
+        }
+
+        private void FormPressureTester_KeyDown(object sender, KeyEventArgs e)
+        {
+        }
+
+        private void FormPressureTester_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
         }
     }
 }
