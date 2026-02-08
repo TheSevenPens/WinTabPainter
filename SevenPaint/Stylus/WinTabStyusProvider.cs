f1d36377703093e4a65ba5ace21e1528265c542f
TheSevenPens <98374109+TheSevenPens@users.noreply.github.com>
2026-02-07 19:40:13 -0800

    Created function to create StylusEventArgs from WinTabPacket

--- SevenPaint/Stylus/WinTabStyusProvider.cs
+++ SevenPaint/Stylus/WinTabStyusProvider.cs
@@ -50,41 +50,46 @@ namespace SevenPaint.Stylus
             _targetElement.Dispatcher.Invoke(() =>
             {
                 if (!IsActive) return;
-
-                // WinTab gives us screen coordinates
-                // For a "drawing app" so we need to convert them to local coordinates relative to the target element (canvas)
-                var localpos = _targetElement.PointFromScreen(new System.Windows.Point(packet.pkX, packet.pkY));
-
-                // Pen Orientation is given in Azimuth (0-360), Altitude (0-90), and Twist (0-360)
-                // WinTab gives these values in tenths of degrees, so we divide by 10 to get actual degrees
-                double angle_normalization_factor = 10.0;
-                double azimuth = packet.pkOrientation.orAzimuth / angle_normalization_factor;
-                double altitude = packet.pkOrientation.orAltitude / angle_normalization_factor;
-                double twist = packet.pkOrientation.orTwist / angle_normalization_factor;
-
-                // WinTab provides the orientation only in Azimuth/Altitude format
-                // For convenience, convert that to XY tilt
-                var tiltaa_deg = new SevenUtils.Trigonometry.TiltAA(azimuth, altitude);
-                var tiltxy_deg = tiltaa_deg.ToXY_Deg();
-
-                // Create Args
-                var args = new StylusEventArgs
-                {
-                    ScreenPos = new SevenUtils.Geometry.PointD(packet.pkX, packet.pkY),
-                    LocalPos = new SevenUtils.Geometry.PointD(localpos.X, localpos.Y),
-                    HoverDistance = packet.pkZ,
-                    PressureLevelRaw = packet.pkNormalPressure,
-                    PressureNormalized = packet.pkNormalPressure / (float)_session.TabletInfo.MaxPressure,
-                    TiltAADeg = tiltaa_deg,
-                    TiltXYDeg = tiltxy_deg,
-                    Twist = twist,
-                    ButtonsRaw = (int)packet.pkButtons,
-                    Timestamp = packet.pkTime
-                };
-
+                var args = CreateStylusArgsFromPacket(packet);
                 InputMove?.Invoke(args);
-                
+
             });
+        }
+
+        private StylusEventArgs CreateStylusArgsFromPacket(WinTabDN.Structs.WintabPacket packet)
+        {
+
+            // WinTab gives us screen coordinates
+            // For a "drawing app" so we need to convert them to local coordinates relative to the target element (canvas)
+            var localpos = _targetElement.PointFromScreen(new System.Windows.Point(packet.pkX, packet.pkY));
+
+            // Pen Orientation is given in Azimuth (0-360), Altitude (0-90), and Twist (0-360)
+            // WinTab gives these values in tenths of degrees, so we divide by 10 to get actual degrees
+            double angle_normalization_factor = 10.0;
+            double azimuth = packet.pkOrientation.orAzimuth / angle_normalization_factor;
+            double altitude = packet.pkOrientation.orAltitude / angle_normalization_factor;
+            double twist = packet.pkOrientation.orTwist / angle_normalization_factor;
+
+            // WinTab provides the orientation only in Azimuth/Altitude format
+            // For convenience, convert that to XY tilt
+            var tiltaa_deg = new SevenUtils.Trigonometry.TiltAA(azimuth, altitude);
+            var tiltxy_deg = tiltaa_deg.ToXY_Deg();
+
+            // Create Args
+            var args = new StylusEventArgs
+            {
+                ScreenPos = new SevenUtils.Geometry.PointD(packet.pkX, packet.pkY),
+                LocalPos = new SevenUtils.Geometry.PointD(localpos.X, localpos.Y),
+                HoverDistance = packet.pkZ,
+                PressureLevelRaw = packet.pkNormalPressure,
+                PressureNormalized = packet.pkNormalPressure / (float)_session.TabletInfo.MaxPressure,
+                TiltAADeg = tiltaa_deg,
+                TiltXYDeg = tiltxy_deg,
+                Twist = twist,
+                ButtonsRaw = (int)packet.pkButtons,
+                Timestamp = packet.pkTime
+            };
+            return args;
         }
     }
 }
