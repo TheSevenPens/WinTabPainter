a1e9b7fc301c205e6e047c2068c25fe0f4f787d8
TheSevenPens <98374109+TheSevenPens@users.noreply.github.com>
2026-02-13 04:57:07 -0800

    Reorganized code

--- WinTabHelloWorld/DrawingState.cs
+++ WinTabHelloWorld/DrawingState.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Windows;
+﻿using System.Windows;
 
 namespace WinTabHelloWorld;
 
@@ -10,25 +9,4 @@ public class DrawingState
     public PointerData PointerData;
 
 
-}
-
-public struct PointerData
-{
-    // Meta
-    public DateTime Time { get; set; }
-    
-    // Position
-
-    public SevenLib.Geometry.PointD DisplayPoint { get; set; }
-    public SevenLib.Geometry.PointD CanvasPoint { get; set; }
-    public double Height { get; set; }
-    public double PressureNormalized { get; set; }
-
-    // Orientation (Tilt & Twist)
-    public SevenLib.Trigonometry.TiltXY TiltXYDeg { get; set; }
-    public SevenLib.Trigonometry.TiltAA TiltAADeg { get; set; }
-    public double Twist { get; set; } // Barrel Rotation
-
-    // Buttons
-    public SevenLib.WinTab.Utils.StylusButtonState ButtonState { get; set; }
 }
---
+++ WinTabHelloWorld/PointerData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace WinTabHelloWorld;
+
+public struct PointerData
+{
+    // Meta
+    public DateTime Time { get; set; }
+    
+    // Position
+
+    public SevenLib.Geometry.PointD DisplayPoint { get; set; }
+    public SevenLib.Geometry.PointD CanvasPoint { get; set; }
+    public double Height { get; set; }
+    public double PressureNormalized { get; set; }
+
+    // Orientation (Tilt & Twist)
+    public SevenLib.Trigonometry.TiltXY TiltXYDeg { get; set; }
+    public SevenLib.Trigonometry.TiltAA TiltAADeg { get; set; }
+    public double Twist { get; set; } // Barrel Rotation
+
+    // Buttons
+    public SevenLib.WinTab.Utils.StylusButtonState ButtonState { get; set; }
+}
