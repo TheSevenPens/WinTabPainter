5f36259225cae3f8afddadaece5d45c0ece454f0
TheSevenPens <98374109+TheSevenPens@users.noreply.github.com>
2026-02-16 00:01:06 -0800

    SevenPaint moved to new TabletSession

--- SevenPaint/Stylus/WinTabStyusProvider.cs
+++ SevenPaint/Stylus/WinTabStyusProvider.cs
@@ -6,7 +6,7 @@ namespace SevenPaint.Stylus
 {
     public class WinTabStyusProvider
     {
-        private SevenLib.WinTab.Tablet.WinTabSession _session;
+        private SevenLib.WinTab.Tablet.WinTabSession2 _session;
         private FrameworkElement _targetElement;
         
 #pragma warning disable 67
@@ -19,7 +19,7 @@ namespace SevenPaint.Stylus
         public WinTabStyusProvider(FrameworkElement targetElement)
         {
             _targetElement = targetElement;
-            _session = new SevenLib.WinTab.Tablet.WinTabSession();
+            _session = new SevenLib.WinTab.Tablet.WinTabSession2();
             _session.OnRawPacketReceived = OnWintabPacket;
             _session.OnButtonStateChanged = OnButtonChanged;
         }
