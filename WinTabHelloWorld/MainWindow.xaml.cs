3731391d0d710bd6ef02ce5dd1e584655fdf93d8
TheSevenPens <98374109+TheSevenPens@users.noreply.github.com>
2026-02-15 20:15:19 -0800

    Renamed class

--- WinTabHelloWorld/MainWindow.xaml.cs
+++ WinTabHelloWorld/MainWindow.xaml.cs
@@ -12,7 +12,7 @@ public partial class MainWindow : Window
 {
     private SevenLib.WinTab.Tablet.TabletSession _wintabsession;
     private SevenLib.Media.CanvasRenderer _renderer;
-    private DrawingState _drawingState = new DrawingState();
+    private PointerState _drawingState = new PointerState();
     private string _buttonStatus = "None";
     private const int DefaultCanvasWidth = 800;
     private const int DefaultCanvasHeight = 600;
--- WinTabHelloWorld/PointerState.cs
+++ WinTabHelloWorld/PointerState.cs
@@ -1,12 +1,10 @@
-﻿using System.Windows;
-
+﻿
 namespace WinTabHelloWorld;
 
-public class DrawingState
+public class PointerState
 {
     public SevenLib.WinTab.Structs.WintabPacket WinTabPacket { get; set; }
 
     public SevenLib.Stylus.PointerData PointerData;
-
 
 }
