53c86a7b829091e4c0e1e7e6e99327c46c54b1b5
TheSevenPens <98374109+TheSevenPens@users.noreply.github.com>
2026-02-15 20:35:30 -0800

    renamed member

--- WinTabHelloWorld/MainWindow.xaml.cs
+++ WinTabHelloWorld/MainWindow.xaml.cs
@@ -12,7 +12,7 @@ public partial class MainWindow : Window
 {
     private SevenLib.WinTab.Tablet.TabletSession _wintabsession;
     private SevenLib.Media.CanvasRenderer _renderer;
-    private SevenLib.WinTab.Tablet.PointerState _drawingState = new SevenLib.WinTab.Tablet.PointerState();
+    private SevenLib.WinTab.Tablet.PointerState _pointerState = new SevenLib.WinTab.Tablet.PointerState();
     private string _buttonStatus = "None";
     private const int DefaultCanvasWidth = 800;
     private const int DefaultCanvasHeight = 600;
@@ -54,27 +54,27 @@ public partial class MainWindow : Window
         {
             try
             {
-                _drawingState.WinTabPacket = packet;
-                _drawingState.PointerData = new SevenLib.Stylus.PointerData();
-                _drawingState.PointerData.Time = DateTime.Now;
+                _pointerState.WinTabPacket = packet;
+                _pointerState.PointerData = new SevenLib.Stylus.PointerData();
+                _pointerState.PointerData.Time = DateTime.Now;
 
                 var screenPos = new Point(packet.pkX, packet.pkY);
                 var canvasPos = CanvasImage.PointFromScreen(screenPos);
 
-                _drawingState.PointerData.DisplayPoint= new SevenLib.Geometry.PointD(screenPos.X, screenPos.Y);
-                _drawingState.PointerData.CanvasPoint = new SevenLib.Geometry.PointD(canvasPos.X, canvasPos.Y) ;
-                _drawingState.PointerData.Height = packet.pkZ;
+                _pointerState.PointerData.DisplayPoint= new SevenLib.Geometry.PointD(screenPos.X, screenPos.Y);
+                _pointerState.PointerData.CanvasPoint = new SevenLib.Geometry.PointD(canvasPos.X, canvasPos.Y) ;
+                _pointerState.PointerData.Height = packet.pkZ;
                 float normalized_pressure = (float)packet.pkNormalPressure / _wintabsession.TabletInfo.MaxPressure;
-                _drawingState.PointerData.PressureNormalized = normalized_pressure;
-                _drawingState.PointerData.TiltAADeg = new SevenLib.Trigonometry.TiltAA(packet.pkOrientation.orAzimuth/10, packet.pkOrientation.orAltitude/10);
-                _drawingState.PointerData.TiltXYDeg = _drawingState.PointerData.TiltAADeg.ToXY_Deg();
-                _drawingState.PointerData.Twist = packet.pkOrientation.orTwist;
-                _drawingState.PointerData.ButtonState = this._wintabsession.StylusButtonState;
+                _pointerState.PointerData.PressureNormalized = normalized_pressure;
+                _pointerState.PointerData.TiltAADeg = new SevenLib.Trigonometry.TiltAA(packet.pkOrientation.orAzimuth/10, packet.pkOrientation.orAltitude/10);
+                _pointerState.PointerData.TiltXYDeg = _pointerState.PointerData.TiltAADeg.ToXY_Deg();
+                _pointerState.PointerData.Twist = packet.pkOrientation.orTwist;
+                _pointerState.PointerData.ButtonState = this._wintabsession.StylusButtonState;
 
-                if (_drawingState.PointerData.PressureNormalized > 0)
+                if (_pointerState.PointerData.PressureNormalized > 0)
                 {
                     const double brush_size = 15;
-                    _renderer.DrawPoint((int)canvasPos.X, (int)canvasPos.Y, (float) (_drawingState.PointerData.PressureNormalized * brush_size));
+                    _renderer.DrawPoint((int)canvasPos.X, (int)canvasPos.Y, (float) (_pointerState.PointerData.PressureNormalized * brush_size));
                 }
             }
             catch (Exception ex)
@@ -92,18 +92,18 @@ public partial class MainWindow : Window
     private void UpdatePointerStats(object sender, EventArgs e)
     {
         // Update UI with last packet info
-        if ((DateTime.Now - _drawingState.PointerData.Time).TotalSeconds < 1.0)
+        if ((DateTime.Now - _pointerState.PointerData.Time).TotalSeconds < 1.0)
         {
-            ValGx.Text = _drawingState.PointerData.DisplayPoint.X.ToString();
-            ValGy.Text = _drawingState.PointerData.DisplayPoint.Y.ToString();
-            ValCx.Text = _drawingState.PointerData.CanvasPoint.X.ToString("F0");
-            ValCy.Text = _drawingState.PointerData.CanvasPoint.Y.ToString("F0");
+            ValGx.Text = _pointerState.PointerData.DisplayPoint.X.ToString();
+            ValGy.Text = _pointerState.PointerData.DisplayPoint.Y.ToString();
+            ValCx.Text = _pointerState.PointerData.CanvasPoint.X.ToString("F0");
+            ValCy.Text = _pointerState.PointerData.CanvasPoint.Y.ToString("F0");
 
-            ValZ.Text = _drawingState.PointerData.Height.ToString();
-            ValP.Text = _drawingState.PointerData.PressureNormalized.ToString();
+            ValZ.Text = _pointerState.PointerData.Height.ToString();
+            ValP.Text = _pointerState.PointerData.PressureNormalized.ToString();
 
-            ValAz.Text = _drawingState.PointerData.TiltAADeg.Azimuth.ToString();
-            ValAlt.Text = _drawingState.PointerData.TiltAADeg.Altitude.ToString();
+            ValAz.Text = _pointerState.PointerData.TiltAADeg.Azimuth.ToString();
+            ValAlt.Text = _pointerState.PointerData.TiltAADeg.Altitude.ToString();
 
             ValBtn.Text = _buttonStatus;
         }
